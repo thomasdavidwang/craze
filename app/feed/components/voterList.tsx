62474b51089ff06fac2aa4730bfba8d339f857e5
thomasdavidwang <wang.thomasdavid@gmail.com>
2023-11-12 00:28:37 -0500

    actually optimized fetch voter list

--- app/feed/components/voterList.tsx
+++ app/feed/components/voterList.tsx
@@ -5,7 +5,12 @@ import Stack from "@mui/material/Stack";
 import ProfileImage from "./profileImage";
 import * as queries from "@/src/graphql/queries";
 import { GraphQLQuery } from "@aws-amplify/api";
-import { GetUserQuery, ListUsersQuery, ModelUserFilterInput } from "@/src/API";
+import {
+  GetUserQuery,
+  GetVoteQuery,
+  ListUsersQuery,
+  ModelUserFilterInput,
+} from "@/src/API";
 import Typography from "@mui/material/Typography";
 import { useEffect, useState } from "react";
 import { API } from "aws-amplify";
@@ -13,39 +18,31 @@ import { API } from "aws-amplify";
 export default function VoterList({ dare }) {
   const [voters, setVoters] = useState([]);
 
-  async function getVoters() {
-    const sortedList = dare.Votes.items[0].voters.items.toSorted(
-      (voteA, voteB) => {
-        let dateA = new Date(voteA.createdAt).getTime();
-        let dateB = new Date(voteB.createdAt).getTime();
-        return dateB - dateA;
-      }
-    );
-
-    const userFilters: ModelUserFilterInput[] = sortedList.map((userVote) => {
-      return {
-        id: {
-          eq: userVote.userId,
-        },
-      };
-    });
-
-    console.log(userFilters);
-
+  async function getVoters(Vote) {
     try {
-      const voterList = await API.graphql<GraphQLQuery<ListUsersQuery>>({
-        query: queries.listUsers,
-        variables: { or: userFilters },
+      const voterList = await API.graphql<GraphQLQuery<GetVoteQuery>>({
+        query: queries.getVote,
+        variables: { id: Vote.id },
       });
 
-      setVoters(voterList.data.listUsers.items);
+      const temp = voterList.data.getVote.voters.items.toSorted(
+        (uservote1, uservote2) => {
+          let date1 = new Date(uservote1.createdAt).getTime();
+          let date2 = new Date(uservote2.createdAt).getTime();
+
+          return date1 - date2;
+        }
+      );
+
+      // @ts-ignore is not assignable to parameter of type 'SetStateAction<any[]>'
+      setVoters(temp);
     } catch (error) {
       console.log(error);
     }
   }
 
   useEffect(() => {
-    getVoters();
+    getVoters(dare.Votes.items[0]);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [dare]);
 
@@ -53,7 +50,10 @@ export default function VoterList({ dare }) {
     <Stack sx={{ borderTop: 1, borderColor: "grey.800", py: 2 }} spacing={1}>
       {voters[0] && (
         <Typography variant="h4" fontWeight="bold">
-          {"Dared by: " + voters[0].firstName + " " + voters[0].lastName}
+          {"Dared by: " +
+            voters[0].user.firstName +
+            " " +
+            voters[0].user.lastName}
         </Typography>
       )}
       <Typography variant="h4" fontWeight="bold">
@@ -63,7 +63,7 @@ export default function VoterList({ dare }) {
         {voters.map(
           (voter, idx) =>
             voter && (
-              <Grid item key={voter.profilePicKey} xs={4}>
+              <Grid item key={voter.user.id} xs={4}>
                 <Stack
                   direction="row"
                   alignItems="center"
@@ -71,13 +71,13 @@ export default function VoterList({ dare }) {
                   spacing={1}
                 >
                   <ProfileImage
-                    src={voter}
+                    src={voter.user}
                     width={36}
                     height={36}
                     className="rounded-full object-cover max-h-8 max-w-8"
                   />
                   <Typography variant="h4">
-                    {voter.firstName + " " + voter.lastName}
+                    {voter.user.firstName + " " + voter.user.lastName}
                   </Typography>
                 </Stack>
               </Grid>
